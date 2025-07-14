36aa0345c66f1ca88ee32f1e664f5906dcd98fef
victorarruda <victor.leop@gmail.com>
2025-07-14 16:29:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,59 @@
 #include <stdio.h>
-#include <stdio_ext.h>
+//#include <stdio_ext.h>
+#include <string.h>
 #define TAM 3
+
+int lerInteiro(){
+    char buffer[50];
+    int valor;
+    fgets(buffer,sizeof(buffer),stdin);
+    sscanf(buffer,"%d",&valor);
+    return valor;
+}
+
+int lerFloat(){
+    char buffer[50];
+    float valor;
+    fgets(buffer,sizeof(buffer),stdin);
+    sscanf(buffer,"%f",&valor);
+    return valor;
+}
+
+void lerString(char *destino, int tamanho){
+    fgets(destino, tamanho, stdin);
+    destino[strcspn(destino,"\n")] ='\0';
+}
 
 struct Produtos{
     int codigo;
     int quantidade;
-    char nome[15];
+    char nome[50];
     float preco;
 };
 
 int main() {
  //   struct Produto;
+    char buffer[50];
     int opcao,indice=0;
     struct Produtos produto[TAM];
     printf("1 - Cadastrar produto. \n");
     printf("2 - Listar produtos. \n");
     printf("3 - Efetuar pedido. \n");
     printf("4 - Listar pedidos. \n");
-    scanf("%d",&opcao);
+    opcao = lerInteiro();
     switch(opcao){
         case 1:
             for(int i=indice;i<TAM;i++){
                 //if(indice<TAM){
                     produto[i].codigo=i+1;
                     printf("Código produto: %d - Informe o nome do produto:\n",produto[i].codigo);
-                    __fpurge(stdin);
-                    fgets(produto[i].nome,sizeof(produto[i].nome),stdin);
+                    lerString(produto[i].nome,sizeof(produto[i].nome));
+
                     printf("Informe a quantidade do produto:\n");
-                    __fpurge(stdin);
-                    scanf("%d",&produto[i].quantidade);
+                    produto[i].quantidade=lerInteiro();
+
                     printf("Informe o preço do produto:\n");
-                    __fpurge(stdin);
-                    scanf("%.2f",&produto[i].preco);
+                    produto[i].preco=lerFloat();
                     indice++;
                 //}
             }
